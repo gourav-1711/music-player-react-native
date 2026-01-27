77f8c5e9d6c45691066feba72ec20c5927003e53
gourav-1711 <gouravdadhich34@gmail.com>
2026-01-27 13:02:02 +0530

    remove react native track player

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,11 +10,10 @@ import "react-native-reanimated";
 import useFavourite from "@/hooks/store/favourite";
 import useHistory from "@/hooks/store/history";
 import usePlaylist from "@/hooks/store/playlist";
-import { setupPlayer, updateOptions } from "@/hooks/trackPlayerServices";
+
 import { useColorScheme } from "@/hooks/use-color-scheme";
 import { setStyle } from "expo-navigation-bar";
-import { useEffect, useState } from "react";
-import { View } from "react-native";
+import { useEffect } from "react";
 import { PaperProvider } from "react-native-paper";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
@@ -28,15 +27,6 @@ export default function RootLayout() {
   const loadPlaylists = usePlaylist((state) => state.loadData);
   const loadHistory = useHistory((state) => state.loadData);
   const loadFavourite = useFavourite((state) => state.loadSongs);
-  const [isPlayerReady, setIsPlayerReady] = useState(false);
-
-  async function setup() {
-    const isSetup = await setupPlayer();
-    if (isSetup) {
-      await updateOptions();
-      setIsPlayerReady(true);
-    }
-  }
 
   useEffect(() => {
     loadSongs();
@@ -44,12 +34,7 @@ export default function RootLayout() {
     loadHistory();
     loadFavourite();
     setStyle("auto");
-    setup();
   }, []);
-
-  if (!isPlayerReady) {
-    return <View>loading...</View>;
-  }
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
